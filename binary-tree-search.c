8dd5a7a77d1be777ff78a61ba76531640a52295f
Pedro Lages <55694628+pxd3v@users.noreply.github.com>
2020-10-20 14:36:22 -0300

    * improves binary tree serch exercise

--- binary-tree-search.c
+++ binary-tree-search.c
@@ -37,16 +37,43 @@ node* newNode(int nodeValue)
     return tmp;
 }
 
-int main (void) {
-    node *root = newNode(4);
-    root->left = newNode(2);
-    root->right = newNode(6);
-    root->left->left = newNode(1);
-    root->left->right = newNode(3);
-    root->right->left = newNode(5);
-    root->right->right = newNode(7);
+node* insertNode(node *node, int nodeValue) {
+    if (node == NULL) return newNode(nodeValue); 
+  
+    if (nodeValue < node->value) 
+        node->left  = insertNode(node->left, nodeValue); 
+    else if (nodeValue > node->value) 
+        node->right = insertNode(node->right, nodeValue);    
+  
+    return node; 
+}
 
-    bool result = search(root, 8);
+void printSortedTree(node *node) {
+    if (node == NULL) {
+      return;
+    } 
+    printSortedTree(node->left);
+    printf("%d\n", node->value);
+    printSortedTree(node->right);
+}
+
+node* mountTree(node *root, int size) {
+    for(int i = 0; i < size; i++){
+        if(i == 0) {
+            root = insertNode(root, i + 1);
+        }
+        insertNode(root, i + 1);
+    }
+    return root;
+}
+
+int main (void) {
+    node *root = NULL;
+    int sizeOfTree = 8;
+    int *numbers = malloc(sizeOfTree * sizeof(int));
+    root = mountTree(root, sizeOfTree);
+    printSortedTree(root);
+    bool result = search(root, 11);
     if(result){
         printf("Achou!\n");
     } else {
